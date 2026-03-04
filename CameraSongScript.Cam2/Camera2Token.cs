b196516436ccf7077bcb30f60700a82ba1bfea9a
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-03-05 00:04:36 +0900

    Camera2のエラー修正＆動作確認

--- CameraSongScript.Cam2/Camera2Token.cs
+++ CameraSongScript.Cam2/Camera2Token.cs
@@ -28,6 +28,13 @@ namespace CameraSongScript.Cam2
             _token.rotation = rot;
         }
 
+        public void SetPositionAndRotation(Vector3 pos, Vector3 rot)
+        {
+            _token.position = pos;
+            _token.rotation = rot;
+            _token.UpdatePositionAndRotation();
+        }
+
         public float GetFOV()
         {
             return _token.FOV;
@@ -43,24 +50,31 @@ namespace CameraSongScript.Cam2
             var go = _token.visibleObjects;
             if (go == null) return;
 
-            if (scriptVisible.avatar.HasValue)
-                go.Avatar = scriptVisible.avatar.Value ? AvatarVisibility.Visible : AvatarVisibility.Hidden;
-            if (scriptVisible.notes.HasValue)
-                go.Notes = scriptVisible.notes.Value ? NoteVisibility.Visible : NoteVisibility.Hidden;
-            if (scriptVisible.ui.HasValue)
-                go.UI = scriptVisible.ui.Value;
-            if (scriptVisible.saber.HasValue)
-                go.Sabers = scriptVisible.saber.Value;
-            if (scriptVisible.debris.HasValue)
-                go.Debris = scriptVisible.debris.Value;
-            if (scriptVisible.wall.HasValue)
-                go.Walls = scriptVisible.wall.Value ? WallVisiblity.Visible : WallVisiblity.Hidden;
-            if (scriptVisible.wallFrame.HasValue && scriptVisible.wallFrame.Value)
-                go.Walls = WallVisiblity.Transparent;
-            if (scriptVisible.cutParticles.HasValue)
-                go.CutParticles = scriptVisible.cutParticles.Value;
+            try
+            {
+                if (scriptVisible.avatar.HasValue)
+                    go.Avatar = scriptVisible.avatar.Value ? AvatarVisibility.Visible : AvatarVisibility.Hidden;
+                if (scriptVisible.notes.HasValue)
+                    go.Notes = scriptVisible.notes.Value ? NoteVisibility.Visible : NoteVisibility.Hidden;
+                if (scriptVisible.ui.HasValue)
+                    go.UI = scriptVisible.ui.Value;
+                if (scriptVisible.saber.HasValue)
+                    go.Sabers = scriptVisible.saber.Value;
+                if (scriptVisible.debris.HasValue)
+                    go.Debris = scriptVisible.debris.Value;
+                if (scriptVisible.wall.HasValue)
+                    go.Walls = scriptVisible.wall.Value ? WallVisiblity.Visible : WallVisiblity.Hidden;
+                if (scriptVisible.wallFrame.HasValue && scriptVisible.wallFrame.Value)
+                    go.Walls = WallVisiblity.Transparent;
+                if (scriptVisible.cutParticles.HasValue)
+                    go.CutParticles = scriptVisible.cutParticles.Value;
 
-            _token.UpdateVisibleObjects();
+                _token.UpdateVisibleObjects();
+            }
+            catch (System.Exception)
+            {
+                // Camera2側でGameObjectsが完全に初期化されていない場合にNullReferenceExceptionが発生することがあるため無視する
+            }
         }
 
         public void ResetVisibleObjects()
@@ -68,15 +82,22 @@ namespace CameraSongScript.Cam2
             var go = _token.visibleObjects;
             if (go == null) return;
 
-            go.Avatar = AvatarVisibility.Visible;
-            go.Notes = NoteVisibility.Visible;
-            go.UI = true;
-            go.Sabers = true;
-            go.Debris = true;
-            go.Walls = WallVisiblity.Visible;
-            go.CutParticles = true;
+            try
+            {
+                go.Avatar = AvatarVisibility.Visible;
+                go.Notes = NoteVisibility.Visible;
+                go.UI = true;
+                go.Sabers = true;
+                go.Debris = true;
+                go.Walls = WallVisiblity.Visible;
+                go.CutParticles = true;
 
-            _token.UpdateVisibleObjects();
+                _token.UpdateVisibleObjects();
+            }
+            catch (System.Exception)
+            {
+                // 無視する
+            }
         }
 
         public void Dispose()
