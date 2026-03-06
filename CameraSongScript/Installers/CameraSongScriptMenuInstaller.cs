ec8e399c171e6c1219bd5cb2c65d7df7b622051c
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-03-06 16:31:11 +0900

    fix(UI): 譜面切り替え時にScript Fileドロップダウンの表示が更新されない不具合を修正
    - CameraSongScriptSettingsView.bsml のドロップダウンに bind-value="true" を追加
    - CameraSongScriptSettingsView.cs の OnScanCompleted における更新順序を整理
    - ドロップダウン更新時に ReceiveValue() を呼び出し、C#側の値をUIに強制同期
    - CameraSongScriptMenuInstaller.cs のテスト用FlowCoordinatorをDEBUG時のみに制限

--- CameraSongScript/Installers/CameraSongScriptMenuInstaller.cs
+++ CameraSongScript/Installers/CameraSongScriptMenuInstaller.cs
@@ -8,7 +8,9 @@ namespace CameraSongScript.Installers
     {
         public override void InstallBindings()
         {
+#if DEBUG
             this.Container.BindInterfacesAndSelfTo<BSMLtestFlowCoordinator>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+#endif
             this.Container.BindInterfacesAndSelfTo<CameraSongScriptSettingsView>().FromNewComponentAsViewController().AsSingle().NonLazy();
             this.Container.BindInterfacesAndSelfTo<CameraSongScriptStatusView>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
             this.Container.BindInterfacesAndSelfTo<LevelSelectionDetector>().AsSingle().NonLazy();
