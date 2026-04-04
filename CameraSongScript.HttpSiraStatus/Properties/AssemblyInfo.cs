d05e475c1e18746b92e5bbc5eec86a5116d7ad7c
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2026-04-04 22:50:01 +0900

    VersionUP

--- CameraSongScript.HttpSiraStatus/Properties/AssemblyInfo.cs
+++ CameraSongScript.HttpSiraStatus/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("c3d4e5f6-3333-4444-5555-666677778888")]
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyVersion("0.3.3")]
+[assembly: AssemblyFileVersion("0.3.3")]
